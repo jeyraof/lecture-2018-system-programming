6c656bd1e52700ea08dc551870ea3e000946c503
Jaeyoung Lee <jaeyoung@monodiary.net>
2018-11-30 15:04:01 +0900

    2018-11-23 (2)

--- 2018-11-23/file_io.cpp
+++ 2018-11-23/file_io.cpp
@@ -1,49 +1,57 @@
-//**************************************************************
-// 제목: File I/O 예제 
-// 기능: 
-// 파일이름: file_io.cpp
-// 수정날짜: 2018년11월23일
-// 작성자: 이재영
-//**************************************************************
-
-#include <stdlib.h>
+#define UNICODE
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
 #include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+#define READ_SIZE 512
+
+void CheckOsVersion();
 
 int main() {
-	FILE *fp;
-	char buff[] = "I'm just a poor boy. Nobody loves me.";
-	char rBuff[100];
+	BOOL rc;
+	HANDLE hFile;
+	DWORD numread;
+	OVERLAPPED overlap;
+	wchar_t buf[READ_SIZE];
+	wchar_t szPath[MAX_PATH];
 
-	fopen_s(&fp, "test.txt", "r");
-	if (fp == NULL) {
-		printf("Error opening the file\n");
-		return 0;
+	CheckOsVersion();
+
+	GetWindowsDirectory(szPath, sizeof(szPath));
+	wchar_t endpoint[] = L"\\WINHLP32.EXE";
+	wcscat_s(szPath, wcslen(szPath)+wcslen(endpoint)+1, L"\\WINHLP32.EXE");
+	hFile = CreateFile(
+		szPath,
+		GENERIC_READ,
+		FILE_SHARE_READ|FILE_SHARE_WRITE,
+		NULL,
+		OPEN_EXISTING,
+		FILE_FLAG_OVERLAPPED,
+		NULL
+	);
+	if (hFile == INVALID_HANDLE_VALUE) {
+		printf("Could not open %s\n", szPath);
+		return -1;
 	}
-	
-	// Write string to fp
-	//fprintf(fp, "Queen'Bohemian Rapsody : %s\n", buff);
-
-	// Read one word separated by white-space
-	fscanf_s(fp, "%s", rBuff, 100);
-	printf("%s\n", rBuff);
-	
-	// Read one-line from fp
-	fgets(rBuff, 100, fp);
-	printf("%s\n", rBuff);
-
-	fgets(rBuff, 100, fp);
-	printf("%s\n", rBuff);
-
-	//fscanf_s(fp, "%s\n", buff);
-	//printf("1: %s\n", buff);
-
-	//fgets(buff, 255, (FILE*)fp);
-	//printf("2: %s\n", buff);
-
-	//fgets(buff, 255, (FILE*)fp);
-	//printf("3: %s\n", buff);
-	
-	fclose(fp);
 
 	return 0;
+}
+
+void CheckOsVersion()
+{
+    OSVERSIONINFO   ver;
+    BOOL            bResult;
+
+    ver.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+
+    bResult = GetVersionEx((LPOSVERSIONINFO) &ver);
+
+    if ( (!bResult) ||
+         (ver.dwPlatformId != VER_PLATFORM_WIN32_NT) )
+    {
+        fprintf(stderr, "IoByFile must be run under Windows NT.\n");
+		exit(EXIT_FAILURE);
+    }
 }
